581a7bdc78c479228a8f4eb10ea271ab51c9e337
Nate Krinsky <natekrinsky@Nates-MacBook-Pro.local>
2019-04-29 15:15:55 -0400

    moved buffer stuff to separate files

---
+++ Buffer.c
@@ -0,0 +1,127 @@
+/* 
+ * Nate Krinsky and Ethan Tu
+ * High Performance HTTP Proxy
+ *
+ * Buffer.h file
+ *
+ * Purpose: This module stores partial messages
+ * in a buffer so complete packets can be handled
+ */
+
+#include "Buffer.h"
+#include "Bucket.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void server_error(char *msg) {
+        perror(msg);
+        exit(EXIT_FAILURE);
+}
+
+partialBuffer_ptr newPartialBuffer()
+{
+        partialBuffer_ptr buf = malloc(sizeof(*buf));
+        if (buf == NULL) server_error("create partialBuffer_ptr");
+        buf->buffer = malloc(BUFSIZE);
+        memset(buf->buffer, 0, BUFSIZE);
+        if (buf->buffer == NULL) server_error("create partialBuffer_ptr buffer");
+        buf->size = BUFSIZE;
+        buf->length = 0;
+        buf->bucket = new_bucket(BUCKET_SIZE, TOKEN_RATE);
+        return buf;
+}
+
+void insertPartialBuffer(partialBuffer_ptr partial_buffer, char* msg, int length)
+{
+        printf("in insertPartialBuffer\n");
+        if (length >= partial_buffer->size - partial_buffer->length) {
+                int new_size = 2*partial_buffer->size;
+                fprintf(stderr, "length of msg is %d, expanding partial buffer size from %d -> %d\n", length, partial_buffer->size, new_size);
+                partial_buffer->buffer = realloc(partial_buffer->buffer, new_size);
+                partial_buffer->size = new_size;
+        }
+        int position = partial_buffer->length;
+        memcpy(partial_buffer->buffer + position, msg, length);
+        partial_buffer->length += length;
+}
+
+void printPartialBuffer(partialBuffer_ptr partial_buffer)
+{
+        if(partial_buffer == NULL || partial_buffer->buffer == NULL) return;
+        if(partial_buffer->length == 0){
+                fprintf(stderr, "--empty partial buffer--\n");
+                return;
+        }
+        fprintf(stderr, "--------------------------------------\n");
+        for(int i = 0; i < partial_buffer->length; i++){
+                fprintf(stderr, "%c", partial_buffer->buffer[i]);
+        }
+        fprintf(stderr, "--------------------------------------\n");
+}
+
+void deletePartialBuffer(partialBuffer_ptr partial_buffer)
+{
+        printf("in deletePartialBuffer\n");
+        if(partial_buffer == NULL) return;
+        free(partial_buffer->buffer);
+        printf("freed partial_buffer->buffer\n");
+        free(partial_buffer->bucket);
+        printf("freed partial_buffer->bucket\n");
+        free(partial_buffer);
+        printf("freed partial_buffer\n");
+}
+
+bufferList newBufferList()
+{
+        bufferList buffer_list = malloc(sizeof(*buffer_list));
+        if (buffer_list == NULL) server_error("create bufferList");
+        buffer_list->buffers = malloc(BUFLISTSIZE * sizeof(partialBuffer_ptr));
+        memset(buffer_list->buffers, 0, BUFLISTSIZE * sizeof(partialBuffer_ptr));
+        if (buffer_list->buffers == NULL) server_error("create bufferList buffers");
+        buffer_list->size = BUFLISTSIZE;
+        return buffer_list; 
+}
+
+void insertBufferList(bufferList buffer_list, partialBuffer_ptr partial_buffer, int index)
+{
+        while(index >= buffer_list->size){
+                int new_size = 2*buffer_list->size;
+                int new_size_bytes = new_size * sizeof(partialBuffer_ptr);
+                int old_size_bytes = buffer_list->size * sizeof(partialBuffer_ptr);
+                fprintf(stderr, "index is %d, expanding buffer list size from %d -> %d\n", index, buffer_list->size, new_size);
+                partialBuffer_ptr *new_list = malloc(new_size_bytes);
+                memset(new_list, 0, new_size_bytes);
+                memcpy(new_list, buffer_list->buffers, old_size_bytes);
+                free(buffer_list->buffers);
+                buffer_list->buffers = new_list;
+                buffer_list->size = new_size;
+        }
+        buffer_list->buffers[index] = partial_buffer;
+}
+
+void deleteFromBufferList(bufferList buffer_list, int index)
+{
+        fprintf(stderr, "deleting fd: %d from buffer list\n", index);
+        partialBuffer_ptr partial_buffer =  buffer_list->buffers[index];
+        deletePartialBuffer(partial_buffer);
+        buffer_list->buffers[index] = NULL;
+}
+
+void clearFromBufferList(bufferList buffer_list, int index) {
+        fprintf(stderr, "clearing buffer of fd: %d\n", index);
+        partialBuffer_ptr partial_buffer =  buffer_list->buffers[index];
+        deletePartialBuffer(partial_buffer);
+        partialBuffer_ptr new_partial_buffer =  newPartialBuffer();
+        insertBufferList(buffer_list, new_partial_buffer, index);
+}
+
+void printBufferList(bufferList buffer_list)
+{       
+        fprintf(stderr, "%s\n", "printing buffer list");
+        for(int i = 0; i < buffer_list->size; i++){
+                fprintf(stderr, "fd: %d\n", i);
+                partialBuffer_ptr current_buffer = buffer_list->buffers[i];
+                printPartialBuffer(current_buffer);
+        }
+}
---
+++ Buffer.h
@@ -0,0 +1,46 @@
+/* 
+ * Nate Krinsky and Ethan Tu
+ * High Performance HTTP Proxy
+ *
+ * Buffer.h file
+ *
+ * Purpose: This module stores partial messages
+ * in a buffer so complete packets can be handled
+ */
+
+#ifndef BUFFER_H_INCLUDED
+#define BUFFER_H_INCLUDED
+
+#include "Bucket.h"
+
+#define BUFSIZE 2*4096
+#define BUFLISTSIZE 1
+#define BUCKET_SIZE 2*BUFSIZE
+#define TOKEN_RATE 50000
+
+typedef struct partialBuffer {
+        char *buffer;
+        int length;
+        int size;
+        Bucket_ptr bucket;
+}*partialBuffer_ptr;
+
+typedef struct bufferListStruct{
+        partialBuffer_ptr *buffers;
+        int size;
+}*bufferList;
+
+void server_error(char *msg);
+
+partialBuffer_ptr newPartialBuffer();
+void insertPartialBuffer(partialBuffer_ptr partial_buffer, char* msg, int length);
+void printPartialBuffer(partialBuffer_ptr partial_buffer);
+void deletePartialBuffer(partialBuffer_ptr partial_buffer);
+
+bufferList newBufferList();
+void insertBufferList(bufferList buffer_list, partialBuffer_ptr partial_buffer, int index);
+void deleteFromBufferList(bufferList buffer_list, int index);
+void clearFromBufferList(bufferList buffer_list, int index);
+void printBufferList(bufferList buffer_list);
+
+#endif
